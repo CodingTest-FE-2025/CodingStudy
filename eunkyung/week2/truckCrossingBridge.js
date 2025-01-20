bcd394222c8b2a5d99bbbe9dbe3682a2fd3ea734
김은경 <kng4316@gmail.com>
2025-01-20 11:45:31 +0900

    [PG] truckCrossingBridge / lv.2 / X

--- eunkyung/week2/truckCrossingBridge.js
+++ eunkyung/week2/truckCrossingBridge.js
@@ -1,4 +1,35 @@
 function solution(bridge_length, weight, truck_weights) {
   var answer = 0;
+
+  var bridge = [];
+  let sum = 0;
+  let flag = true;
+  for (let i = 0; i < bridge_length; i++) {
+    bridge.push(0);
+  }
+
+  while (flag) {
+    let shiftVal = bridge.shift();
+    sum -= shiftVal;
+    if (sum + truck_weights[0] > weight) {
+      bridge.push(0);
+    } else {
+      sum += truck_weights[0];
+      bridge.push(truck_weights[0]);
+      truck_weights.shift();
+    }
+    answer++;
+    if (truck_weights.length === 0) {
+      for (let i = 0; i < bridge_length; i++) {
+        answer++;
+      }
+      flag = false;
+    }
+  }
+
   return answer;
 }
+
+console.log(solution(2, 10, [7, 4, 5, 6])); //8
+console.log(solution(100, 100, [10])); //101
+console.log(solution(100, 100, [10, 10, 10, 10, 10, 10, 10, 10, 10, 10])); //110
