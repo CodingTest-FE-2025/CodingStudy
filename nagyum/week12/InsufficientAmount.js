4d45241b637be951de23b2cfb28f4fb629d17c3d
nagyum <wildanimal19@naver.com>
2025-04-14 00:40:40 +0900

    [PG]InsufficientAmount/lv.1

---
+++ nagyum/week12/InsufficientAmount.js
@@ -0,0 +1,13 @@
+/** 부족한 금액 계산하기
+ *
+ *
+ */
+function solution(price, money, count) {
+  var answer = 0;
+
+  for (let i = 1; i <= count; i++) {
+    answer += price * i;
+  }
+  answer -= money;
+  return answer > 0 ? answer : 0;
+}
