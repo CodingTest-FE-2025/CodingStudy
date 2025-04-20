7811098a831b96469f7f9165ed060b17b143ddd5
nagyum <wildanimal19@naver.com>
2025-04-20 12:31:00 +0900

    저번 파일들

---
+++ nagyum/week11/Fibonacci.js
@@ -0,0 +1,20 @@
+/** 피보나치
+ * 그냥, 피보나치 개념만 알고 있으면 풀 수 있음
+ */
+
+
+function solution(n) {
+    let answer = 0;
+    let f0 = 0
+    let f1 = 1;
+    
+    for(let i = 2; i <= n; i++){
+        answer = (f0 + f1) % 1234567;
+        f0 = f1;
+        f1 = answer;
+    }
+    
+    return answer;
+}
+
+
--- nagyum/week12/DotProduct.js
+++ nagyum/week12/DotProduct.js
@@ -1,3 +1,8 @@
+/** 내적
+ * 이중 for 문으로 푸는 생각이 먼저 들었는데 시간 복잡도가 그럼 제곱이 되니까.. 
+ * 그래서 생각해보니 같은 길이 일꺼라서 인덱스끼리 비교해서 곱함
+ */
+
 function solution(a, b) {
   let answer = 0;
   for (let i = 0; i < a.length; i++) {
