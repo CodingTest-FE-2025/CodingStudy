3efd246881ff8ad11425403e484453ddbbbb1136
김은경 <kng4316@gmail.com>
2025-06-01 19:47:51 +0900

    [PG] MaxMin / lv.2

--- eunkyung/week19/MaxMin.js
+++ eunkyung/week19/MaxMin.js
@@ -1,31 +1,5 @@
-function solution(n) {
-  let count = 0;
-  let k = 1;
-
-  while ((k * (k - 1)) / 2 < n) {
-    const temp = n - (k * (k - 1)) / 2;
-    if (temp % k === 0) {
-      count++;
-    }
-    k++;
-  }
-
-  return count;
+function solution(s) {
+  const numbers = s.split(" ").map(Number);
+  numbers.sort((a, b) => a - b);
+  return `${numbers[0]} ${numbers[numbers.length - 1]}`;
 }
-
-// function solution(n) {
-//   let count = 0;
-
-//   for (let start = 1; start <= n; start++) {
-//     let sum = 0;
-//     for (let next = start; sum < n; next++) {
-//       sum += next;
-//       if (sum === n) {
-//         count++;
-//         break;
-//       }
-//     }
-//   }
-
-//   return count;
-// }
