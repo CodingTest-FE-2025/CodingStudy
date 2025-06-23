2546e3e1a5f8d88aafcf0654a2f5e4759d063bb5
김은경 <kng4316@gmail.com>
2025-06-24 01:08:12 +0900

    [PG] Caesar / lv.1

--- eunkyung/week21/Caesar.js
+++ eunkyung/week21/Caesar.js
@@ -1,4 +1,14 @@
 function solution(s, n) {
-  var answer = "";
-  return answer;
+  return s
+    .split("")
+    .map((char) => {
+      if (char >= "A" && char <= "Z") {
+        return String.fromCharCode(((char.charCodeAt(0) - 65 + n) % 26) + 65);
+      } else if (char >= "a" && char <= "z") {
+        return String.fromCharCode(((char.charCodeAt(0) - 97 + n) % 26) + 97);
+      } else {
+        return char;
+      }
+    })
+    .join("");
 }
