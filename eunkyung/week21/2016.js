76a407b2cfc29bae4148771fff902e5cde739936
김은경 <kng4316@gmail.com>
2025-06-24 01:04:10 +0900

    [PG] 2016 / lv.1

--- eunkyung/week21/2016.js
+++ eunkyung/week21/2016.js
@@ -1,4 +1,9 @@
 function solution(s, n) {
-  var answer = "";
-  return answer;
+  const list = ["FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"];
+  const countList = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  let count = 0;
+  for (let i = 0; i < s - 1; i++) {
+    count = count + countList[i];
+  }
+  return list[(count + n - 1) % 7];
 }
