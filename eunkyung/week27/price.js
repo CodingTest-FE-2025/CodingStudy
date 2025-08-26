3374eb7b7811ad5f756b14b5429afc5ce00bd3b8
김은경 <kng4316@gmail.com>
2025-08-27 01:32:13 +0900

    [PG] price / lv.2

--- eunkyung/week27/price.js
+++ eunkyung/week27/price.js
@@ -1,4 +1,15 @@
 function solution(prices) {
-  var answer = [];
-  return answer;
+  const result = [];
+  for (let j = 0; j < prices.length; j++) {
+    let count = 0;
+    for (let i = j + 1; i < prices.length; i++) {
+      count++;
+      if (prices[j] > prices[i]) {
+        break;
+      }
+    }
+    result.push(count);
+  }
+
+  return result;
 }
